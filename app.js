6490fff54f9dfb10ce17ed1d0c1a5fad4ab894c3
hendiarji <hendiarg@gmail.com>
2022-04-12 09:56:57 +0700

    wkw

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const router = require("./routes");
+const log = require("./middlewares/logger");
+// const res = require("express/lib/response");
+// const req = require("express/lib/request");
+
+app.use(log);
+app.use(express.urlencoded({ extends: true }));
+app.use(router);
+app.use((req, res, next) => {
+  res.status(404);
+  res.send({
+    status: "failed",
+    message: "Resource" + req.originalUrl + "Not Found",
+  });
+});
+app.listen(3000, () => console.log("server: http://localhost:3000"));
---
+++ routes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  const { page, total } = req.query;
+  res.send({
+    status: "Succesfully",
+    message: "Welcome to Express JS Tutorial",
+    page,
+    total,
+  });
+});
+
+router.get("/product/:id", (req, res) => {
+  res.json({
+    id: req.params.id,
+  });
+});
+
+router.post("/product/", (req, res) => {
+  res.json(req.body);
+});
+
+router.get("/:category/:tag", (req, res) => {
+  const { category, tag } = req.params;
+  res.json({ category, tag });
+});
+
+module.exports = router;
